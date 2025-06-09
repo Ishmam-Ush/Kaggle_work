0282ec39c869d18980e6e293d33f7c41134c48ad
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-09 12:26:45 +0600

    Add files via upload

---
+++ # Kagglw Day 1.py
@@ -0,0 +1,22 @@
+# Kagglw Day 1
+
+print("Hello, World!")
+
+# Aerithmetic and variables 
+print(((1+3)*(2-1)/4)**6)
+
+My_var = 1+5
+print (My_var)
+
+# Multiple variables
+No_of_years = 10
+Days_in_a_year = 365
+Hours_in_a_day = 24
+minures_in_an_hour = 60
+seconds_in_a_miute = 60
+
+
+# Calculate total seconds in a given number of years
+Total_time = No_of_years * Days_in_a_year * Hours_in_a_day * minures_in_an_hour * seconds_in_a_miute
+print(Total_time)
+
