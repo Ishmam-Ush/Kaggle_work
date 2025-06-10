3d306287b05ba3dfcf08a3abbd536f6f6a07e4bd
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-10 09:52:05 +0600

    If else statement

--- # Kaggle Day 2.py
+++ # Kaggle Day 2.py
@@ -7,3 +7,11 @@ def evaluate(temp):
         message = "High fever"
         return message
 print(evaluate(41))  # High fever
+
+# if else statement
+def evaluate_temp(temp):
+    if temp >40:
+        return "High fever"
+    else:
+        return "Normal temperature"
+print(evaluate_temp(39))  # Normal temperature
