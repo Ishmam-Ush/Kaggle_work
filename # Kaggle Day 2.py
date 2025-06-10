ca70fd165f2573b6386cc8769d22503199ba6bad
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-10 13:33:57 +0600

    Years calculation

--- # Kaggle Day 2.py
+++ # Kaggle Day 2.py
@@ -141,4 +141,19 @@ def percentage_liked(ratings):
 # Do not change: should return 0.5
 percentage_liked([1, 2, 3, 4, 5, 4, 5, 1])
 
+# Number of years 
+# TODO: Edit the function
+def percentage_growth(num_users, yrs_ago):
+    growth = (num_users[len(num_users)-1] - num_users[len(num_users)-yrs_ago-1])/num_users[len(num_users)-yrs_ago-1]
+    return growth
 
+# Do not change: Variable for calculating some test examples
+num_users_test = [920344, 1043553, 1204334, 1458996, 1503323, 1593432, 1623463, 1843064, 1930992, 2001078]
+
+# Do not change: Should return .036
+print(percentage_growth(num_users_test, 1))
+
+# Do not change: Should return 0.66
+print(percentage_growth(num_users_test, 7))
+
+# Do not change: Check your answer
