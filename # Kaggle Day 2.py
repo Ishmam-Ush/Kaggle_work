d50cd2223e6ab1760677805aa9868406a8878004
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-10 13:09:46 +0600

    List Exercise

--- # Kaggle Day 2.py
+++ # Kaggle Day 2.py
@@ -96,3 +96,30 @@ def get_phone_bill(gb):
 print(get_phone_bill(10))   # 100
 print(get_phone_bill(20))   # 500
 print(get_phone_bill(30))   # 1000
+
+
+# LIST
+# Do not change: Initial menu for your restaurant
+menu = ['stewed meat with onions', 'bean soup', 'risotto with trout and shrimp',
+       'fish soup with cream and onion', 'gyro']
+
+
+
+# TODO: remove 'bean soup', and add 'roasted beet salad' to the end of the menu
+menu.remove('bean soup')
+menu.append('roasted beet salad')
+
+# Customer Avg
+
+# Do not change: Number of customers each day for the last month
+num_customers = [137, 147, 135, 128, 170, 174, 165, 146, 126, 159,
+                 141, 148, 132, 147, 168, 153, 170, 161, 148, 152,
+                 141, 151, 131, 149, 164, 163, 143, 143, 166, 171]
+
+# TODO: Fill in values for the variables below
+avg_first_seven = sum(num_customers[:7])/7
+avg_last_seven =  sum(num_customers[-7:])/7
+max_month = max(num_customers)
+min_month = min(num_customers)
+
+
