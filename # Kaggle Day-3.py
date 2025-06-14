2d4a62977a658645bdf928224c8fcfecedb85769
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-14 11:31:18 +0600

    Rounding

--- # Kaggle Day-3.py
+++ # Kaggle Day-3.py
@@ -1,1 +1,6 @@
+# Python code to demonstrate the use of the round function
+# The round function is used to round a floating-point number to a specified number of decimal places.
+help(round)  # Display help for the round function
+round(3.141652654, ndigits=2)  # Rounding to 2 decimal places
 round(3.141652654, ndigits=4)  # Rounding to 4 decimal places
+round(3.141652654, ndigits=None)  # Rounding to 0 decimal places
