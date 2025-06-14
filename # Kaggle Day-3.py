6403c9c802d2c70b8d924af6e5ed4adcd4dd2271
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-14 12:18:29 +0600

    Arguments with default values

--- # Kaggle Day-3.py
+++ # Kaggle Day-3.py
@@ -4,3 +4,12 @@ help(round)  # Display help for the round function
 round(3.141652654, ndigits=2)  # Rounding to 2 decimal places
 round(3.141652654, ndigits=4)  # Rounding to 4 decimal places
 round(3.141652654, ndigits=None)  # Rounding to 0 decimal places
+
+# Arguments woth default values
+def greet(who = "Collin"):
+    print("Hello,", who)
+
+greet()
+greet(who="world")
+greet("world")
+
