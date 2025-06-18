da4c188860384767e8c2345256b6dbd8d3d528bf
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-18 13:18:04 +0600

    ex

--- # Kaggle Day 6.py
+++ # Kaggle Day 6.py
@@ -101,3 +101,47 @@ for k in numbers:
 # rjust 
 for planet ,initial in planet_to_initial.items():
     print("{}\'begins with' \"{}\"".format(planet.rjust(10), initial))
+
+# Exercise warmup
+a = ""
+length = len(a)
+
+b = "it's ok"
+length = len(b)
+
+c = 'it\'s ok'
+length = len(c)
+
+d = """hey"""
+length = len(d)
+
+e = '\n'
+length = len(e)
+
+# Exercise 1
+def is_valid_zip(zip_code):
+    """Returns whether the input string is a valid (5 digit) zip code
+    """
+    return True if len(str(zip_code)) == 5 and str.isdigit(zip_code) else False   
+# Ex 2
+def word_search(doc_list, keyword):
+    """
+    Takes a list of documents (each document is a string) and a keyword. 
+    Returns list of the index values into the original list for all documents 
+    containing the keyword.
+
+    Example:
+    doc_list = ["The Learn Python Challenge Casino.", "They bought a car", "Casinoville"]
+    >>> word_search(doc_list, 'casino')
+    >>> [0]
+    """
+    indices = []
+
+    for i, doc in enumerate(doc_list):
+
+        words = doc.split()
+        normalized = [word.rstrip('.,').lower() for word in words]
+
+        if keyword.lower() in normalized:
+            indices.append(i)
+    return indices
