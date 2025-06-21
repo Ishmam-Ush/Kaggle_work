fcee2721149158e794035a192dae0ea431ccd177
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-06-21 13:21:10 +0600

    overloading

--- # Kaggle Day-7.py
+++ # Kaggle Day-7.py
@@ -19,3 +19,20 @@ print("it contains...",
       dir(numpy.random)[-15:]
       )
 
+# Roll Dice
+rolls = numpy.random.randint(low= 1, high= 6, size= 10)
+print(rolls)
+print(rolls.mean)
+print(rolls.tolist)
+# print(help(rolls.mean))
+
+# Operator Overloading
+print(rolls + 10)
+print(rolls<=3)
+# examples
+xl = [[1,2,3],[2,4,6],]
+#Create a 2-dimensional array
+x = numpy.asarray(xl)
+print("xl= {}\nx = \n{}".format(xl,x))
+
+
