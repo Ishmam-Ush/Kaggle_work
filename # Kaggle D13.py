fdc5c626a1e6942b6a309726a2c1a52f25dbd586
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-07-11 21:33:48 +0600

    line chart plotting

--- # Kaggle D13.py
+++ # Kaggle D13.py
@@ -1,3 +1,4 @@
+# Session 1 
 # Data visualization 
 # seaborn
 import pandas as pd
@@ -13,6 +14,25 @@ fifa_data = pd.read_csv(fifa_filepath, index_col="rank_date", parse_dates=True)
 print(fifa_data.head(198))  # Display the first selected rows of the DataFrame
 
 # Set the width and height of the figure
-plt.figure(figsize=(16, 6))
+plt.figure(figsize=(12, 6))
 # Line chart showing how FIFA ranking has changed over time
 sns.lineplot(data=fifa_data)
+plt.title("FIFA Ranking Over Time")
+plt.xlabel("Date")
+plt.ylabel("Ranking")
+
+plt.show()  # Show the plot
+
+# Session 2 
+# Line charts
+
+spotify_filepath = r"E:\Courses\Python Kaggle\Datasets\spotify.csv"
+spotify_data = pd.read_csv(spotify_filepath, index_col="Date", parse_dates=True)
+print(spotify_data.head(10))  # Display the first 10 rows of the DataFrame
+print(spotify_data.tail(10))  # Display the last 10 rows of the DataFrame
+plt.title("Spotify Daily Streams")
+plt.xlabel("Date")
+plt.ylabel("Streams")
+sns.lineplot(data=spotify_data)
+plt.show()  # Show the plot
+
