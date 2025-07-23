52e77b4b1bbe389b4997b4b293a305d96748c885
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-07-23 12:41:35 +0600

    ML1

--- # Kaggle D17 ML.py
+++ # Kaggle D17 ML.py
@@ -7,3 +7,13 @@ print(melb_data.describe())
 print(melb_data.columns)
 melb_data= melb_data.dropna(axis=0)
 print(melb_data.describe())
+y = melb_data.Price # we are aiming to predict the price so this is our prediction target
+melb_features = ['Rooms','Bathroom','Landsize','Lattitude','Longtitude'] # selecting features that will help predict the price
+X = melb_data[melb_features]
+print(X.describe())
+print(X.head())
+
+from sklearn.tree import DecisionTreeRegressor
+
+melb_model = DecisionTreeRegressor(random_state=1) # Step 1 : Defining Model type & Specifying a number for random_state to ensure same results each run
+melb_model.fit(X,y) # Fitting Model
