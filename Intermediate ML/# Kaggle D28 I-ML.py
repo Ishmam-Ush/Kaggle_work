9c4e61f734e32190ea0799108d3fdd4d6efefa89
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-08-13 15:38:29 +0200

    Categorical values handling

--- Intermediate ML/# Kaggle D28 I-ML.py
+++ Intermediate ML/# Kaggle D28 I-ML.py
@@ -84,7 +84,3 @@ print(X_train.shape)
 # Number of missing values in each column of training data
 missing_val_count_by_column = (X_train.isnull().sum())
 print(missing_val_count_by_column[missing_val_count_by_column > 0])
-
-# Number of missing values in each column of training data
-missing_val_count_by_column = (X_train.isnull().sum())
-print(missing_val_count_by_column[missing_val_count_by_column > 0])
