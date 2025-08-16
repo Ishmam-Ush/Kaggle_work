a104cef65c99be75d87fdef11b514876bb7a2cf0
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-08-16 02:46:55 +0200

    I-DL

--- Intro to Deep learning/# Kaggle D34 I-DL.py
+++ Intro to Deep learning/# Kaggle D34 I-DL.py
@@ -1,1 +1,19 @@
-# Intro to Deep Learning
+# Intro to Deep Learning 
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Create a network with 1 linear unit
+model = keras.Sequential([
+    layers.Dense(units=1, input_shape=[3])
+])
+model.summary()
+#from tensorflow import keras
+#from tensorflow.keras import layers
+
+# 3 features in your input vector -> shape=(3,)
+#model = keras.Sequential([
+#    keras.Input(shape=(3,)),
+#    layers.Dense(1)          # one linear unit
+#])
+
+#model.summary()
