bfe0f0bbad65243542b517ea6bc1b1a5c5a87852
Ishmam Ush Shaheed <iushshaheed2@gmail.com>
2025-08-16 03:08:12 +0200

    Multi layer Deep Neural Networks

--- Intro to Deep learning/# Kaggle D35 I-DL.py
+++ Intro to Deep learning/# Kaggle D35 I-DL.py
@@ -1,1 +1,12 @@
 # Deep Neural Networks
+import tensorflow as tf 
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential([
+    # the hidden ReLU layers
+    layers.Dense(units=4, activation='relu', input_shape=[2]),
+    layers.Dense(units=3, activation='relu'),
+    # the linear output layer 
+    layers.Dense(units=1),
+])
